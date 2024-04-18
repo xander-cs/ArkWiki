6a8e6be146b981b41547bf482894d04f600a3f12
Xander Chua <xander.chua@icloud.com>
2024-04-17 20:01:35 -0400

    implemented detail fragment + UI changes

--- app/src/main/java/com/example/arkwiki/categories/CategoriesListFragment.kt
+++ app/src/main/java/com/example/arkwiki/categories/CategoriesListFragment.kt
@@ -21,11 +21,9 @@ class CategoriesListFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         val categories = mutableListOf<Card>()
-        categories.add(Card("Breeding", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/a/a3/Eggs.png/revision/latest?cb=20200807154254"))
+        categories.add(Card("Breeding &\nTaming", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/a/a3/Eggs.png/revision/latest?cb=20200807154254"))
         categories.add(Card("Crafting", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/b/b9/Smithy.png/revision/latest?cb=20150615134739"))
         categories.add(Card("Dinosaurs", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/8/8d/Alpha_T-Rex.png/revision/latest?cb=20190228120718"))
-        categories.add(Card("Taming", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/c/cc/Box_o%27_Chocolates.png/revision/latest?cb=20160228212112"))
-        categories.add(Card("Tips", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/5/5f/Magnifying_Glass.png/revision/latest?cb=20220905113952"))
 
         val adapter = CategoriesAdapter(categories)
         recyclerView.adapter = adapter
--- app/src/main/java/com/example/arkwiki/dinosaurs/Dinosaur.kt
+++ app/src/main/java/com/example/arkwiki/dinosaurs/Dinosaur.kt
@@ -14,17 +14,31 @@ enum class Type {
     LOST,
 }
 
+enum class Diet {
+    CARNIVORE,
+    HERBIVORE,
+    PISCIVORE,;
+
+    override fun toString(): String {
+        return super.toString().lowercase().replaceFirstChar(Char::titlecase)
+    }
+}
+
 data class Dinosaur(
     val name: String,
-    val diet: String,
+    val diet: Diet,
     val temperament: String,
+    val tamingMethod: String,
+    val preferredFood: String,
     val image: String,
+    val roar: String,
     val types: List<Type>,
 )
 
 val dinosaurs = listOf(
-    Dinosaur("Carnotaurus", "Carnivore", "Aggressive", "", listOf(Type.ISLAND, Type.CENTER, Type.DESERT, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.SIMULATION, Type.CRYSTALLINE, Type.COLONY , Type.LOST)),
-    Dinosaur("Rex", "Carnivore", "Aggressive", "", listOf(Type.ISLAND, Type.CENTER, Type.DESERT, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
-    Dinosaur("Spino", "Carnivore", "Territorial", "", listOf(Type.ISLAND, Type.CENTER, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
-    Dinosaur("Stegosaurus", "Herbivore", "Docile", "", listOf(Type.ISLAND, Type.CENTER, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.SIMULATION, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
+    Dinosaur("Baryonyx", Diet.PISCIVORE, "Territorial", "Knockout", "Raw Prime Fish Meat", "https://www.dododex.com/media/creature/baryonyx.png", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/3/3a/Baryonyxsound.ogg/revision/latest?cb=20180403205301", listOf(Type.ISLAND, Type.CENTER, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
+    Dinosaur("Carnotaurus", Diet.CARNIVORE, "Aggressive", "Knockout", "Raw Mutton", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/8/89/Render_Carno.png/revision/latest/scale-to-width-down/1000?cb=20150915100742", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/4/4d/Carnosound.ogg/revision/latest?cb=20180403205908", listOf(Type.ISLAND, Type.CENTER, Type.DESERT, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.SIMULATION, Type.CRYSTALLINE, Type.COLONY , Type.LOST)),
+    Dinosaur("Rex", Diet.CARNIVORE, "Aggressive", "Knockout", "Raw Mutton", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/a/a3/Render_Tyrannosaurus.png/revision/latest/scale-to-width-down/1000?cb=20150915101037", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/9/97/Rexsound.ogg/revision/latest?cb=20180403214649", listOf(Type.ISLAND, Type.CENTER, Type.DESERT, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
+    Dinosaur("Spino", Diet.CARNIVORE, "Territorial", "Knockout", "Raw Prime Fish Meat", "https://www.dododex.com/media/creature/spinosaur.png", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/b/b8/Spinosound.ogg/revision/latest?cb=20180801193016", listOf(Type.ISLAND, Type.CENTER, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
+    Dinosaur("Stegosaurus", Diet.HERBIVORE, "Docile", "Knockout", "Mejoberry", "https://www.dododex.com/media/creature/stegosaurus.png", "https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/b/bc/PW_stego_voc_mumble_01.OGG/revision/latest?cb=20150704192009", listOf(Type.ISLAND, Type.CENTER, Type.NORSE, Type.CORRUPTED, Type.GRAND, Type.SIMULATION, Type.CRYSTALLINE, Type.COLONY, Type.LOST)),
 )
